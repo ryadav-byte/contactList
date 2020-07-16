ce3b6de3675cda4464ece07e452463a78be71c57
Ramakant Yadav <2016meb1140@iitrpr.ac.in>
2020-07-17 00:36:57 +0530

    Add contact into contact list

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
 
+var contacts = [
+    {
+        name: 'Ramakant Yadav',
+        phone: '1234567870'
+    },
+    {
+        name: 'Abhishek Dawas',
+        phone: '9087654321'
+    }
+];
 
 app.get('/', function(req, res){
-    return res.render('home', {title:'Contact List'});
+    return res.render('home', {
+        title:'Contact List',
+        contact_list: contacts
+    });
 });
+
+app.post('/create-contact', function(req, res){
+    contacts.push(req.body);
+    return res.redirect('back');
+})
 
 
 app.listen(port, function(err){
